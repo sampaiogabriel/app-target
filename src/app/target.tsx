8934defb45365b0d3e87aa095a5d045bd23cec5d
Gabriel Sampaio (Pessoal) <gabrielsampaiolima@hotmail.com>
2026-04-05 17:22:27 -0300

    feat: add Input component

--- src/app/target.tsx
+++ src/app/target.tsx
@@ -1,6 +1,7 @@
-import { router } from 'expo-router';
-import { Button, Text, View } from 'react-native';
+import { View } from 'react-native';
 
+import { Button } from '@/components/Button';
+import { Input } from '@/components/Input';
 import { PageHeader } from '@/components/PageHeader';
 
 export default function Target() {
@@ -10,6 +11,14 @@ export default function Target() {
         title="Meta"
         subtitle="Economize para alcançar sua meta financeira."
       />
+
+      <View style={{ marginTop: 32, gap: 24 }}>
+        <Input
+          label="Nome da meta"
+          placeholder="Ex. Viagem para praia, Apple Watch"
+        />
+        <Button title="Salvar" />
+      </View>
     </View>
   );
 }
