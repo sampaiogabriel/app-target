5572f5a8363ccb10edcc873c06ed325f71b08585
Gabriel Sampaio (Pessoal) <gabrielsampaiolima@hotmail.com>
2026-04-05 15:39:43 -0300

    feat: add Separator component

---
+++ src/components/Component/index.tsx
@@ -0,0 +1,7 @@
+import { View } from 'react-native';
+
+import { styles } from './styles';
+
+export function Component() {
+  return <View style={styles.container} />;
+}
--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -3,9 +3,18 @@ import { Text, View } from 'react-native';
 
 import { colors } from '@/theme/colors';
 
+import { Separator } from '../Separator';
 import { styles } from './styles';
 
-export function HomeHeader() {
+export type HomeHeaderProps = {
+  total: string;
+};
+
+type Props = {
+  data: HomeHeaderProps;
+};
+
+export function HomeHeader({ data }: Props) {
   return (
     <LinearGradient
       colors={[colors.blue[500], colors.blue[800]]}
@@ -13,7 +22,10 @@ export function HomeHeader() {
     >
       <View>
         <Text style={styles.label}>Total que você possui:</Text>
+        <Text style={styles.total}>{data.total}</Text>
       </View>
+
+      <Separator color={colors.blue[400]} />
     </LinearGradient>
   );
 }
