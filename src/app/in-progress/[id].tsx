a64b34d648fab29e73cb5502148b8c3f2920d751
Gabriel Sampaio (Pessoal) <gabrielsampaiolima@hotmail.com>
2026-04-05 18:03:22 -0300

    feat: add Transaction Page

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -1,8 +1,11 @@
-import { useLocalSearchParams } from 'expo-router';
+import { router, useLocalSearchParams } from 'expo-router';
 import { View } from 'react-native';
 
+import { Button } from '@/components/Button';
+import { List } from '@/components/List';
 import { PageHeader } from '@/components/PageHeader';
 import { Progress } from '@/components/Progress';
+import { Transaction } from '@/components/Transaction';
 
 const details = {
   current: 'R$ 580,00',
@@ -11,14 +14,30 @@ const details = {
 };
 
 export default function InProgress() {
+  const params = useLocalSearchParams<{ id: string }>();
+
   return (
-    <View style={{ flex: 1, padding: 24 }}>
+    <View style={{ flex: 1, padding: 24, gap: 32 }}>
       <PageHeader
         title="Apple Watch"
         rightButton={{ icon: 'edit', onPress: () => '' }}
       />
 
       <Progress data={details} />
+
+      <List
+        title="Transações"
+        data={[]}
+        renderItem={({ item }) => (
+          <Transaction data={item} onRemove={() => ''} />
+        )}
+        emptyMessage="Nenhuma transação. Toque em uma nova transação para guardar seu primeiro dinheiro."
+      />
+
+      <Button
+        title="Nova transação"
+        onPress={() => router.navigate(`/transaction/${params.id}`)}
+      />
     </View>
   );
 }
--- src/app/transaction/[id].tsx
+++ src/app/transaction/[id].tsx
@@ -1,13 +1,29 @@
-import { router, useLocalSearchParams } from 'expo-router';
-import { Button, Text, View } from 'react-native';
+import { useLocalSearchParams } from 'expo-router';
+import { View } from 'react-native';
+
+import { Button } from '@/components/Button';
+import { CurrencyInput } from '@/components/CurrencyInput';
+import { Input } from '@/components/Input';
+import { PageHeader } from '@/components/PageHeader';
 
 export default function Transaction() {
   const { id } = useLocalSearchParams<{ id: string }>();
 
   return (
     <View style={{ flex: 1, padding: 24 }}>
-      <Text>ID: {id}</Text>
-      <Button title="Voltar" onPress={() => router.back()} />
+      <PageHeader
+        title="Nova transação"
+        subtitle="A cada valor guardado você fica mais próximo da sua meta. Se esforce para guardar e evitar retirar"
+      />
+
+      <View style={{ marginTop: 32, gap: 24 }}>
+        <CurrencyInput value={0} label="Valor (R$)" />
+        <Input
+          label="Motivo (opcional)"
+          placeholder="Investir em CDB 100% no banco XPTO"
+        />
+        <Button title="Salvar" />
+      </View>
     </View>
   );
 }
